41ea95e81dd335f9db4833f8c47958659c9f3283
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-06-08 00:20:48 -0600

    some styling changes

--- p-3/client/script.js
+++ p-3/client/script.js
@@ -1,4 +1,4 @@
-document.getElementById('loginBtn').addEventListener('click', (e) => {
+document.getElementById('loginBtn').addEventListener('click', async (e) => {
     e.preventDefault();
 
     const username = document.getElementById('username').value;
@@ -8,4 +8,25 @@ document.getElementById('loginBtn').addEventListener('click', (e) => {
         username: username,
         email: email
     }
+
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(userData)
+    });
+
+    if(!response.ok) {
+        throw new Error(`Server is not responding: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // after login succesfully direct user to tasks page
+    window.location.href = "tasks.html";
+    
+    } catch (error) {
+        console.error('Error Posting data to server', error);
+    }
+    
+
 })
