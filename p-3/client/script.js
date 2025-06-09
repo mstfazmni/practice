a658aacde88ffccee190ff003e5d330a76e47431
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-06-08 22:46:31 -0600

    login endpoint is created so whenever a user info is entered if it is matched with one in the database it'll send the user to tasks page!

--- p-3/client/script.js
+++ p-3/client/script.js
@@ -4,28 +4,26 @@ document.getElementById('loginBtn').addEventListener('click', async (e) => {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
 
-    const userData = {
-        username: username,
-        email: email
-    }
 
     try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        const response = await fetch('http://localhost:3000/login', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(userData)
+        body: JSON.stringify({username, email})
     });
 
     if(!response.ok) {
-        throw new Error(`Server is not responding: ${response.status}`);
+        throw new Error(`Login Failed: ${response.status}`);
     }
 
     const data = await response.json();
+    console.log('Login Success: ', data);
+
     // after login succesfully direct user to tasks page
     window.location.href = "tasks.html";
     
     } catch (error) {
-        console.error('Error Posting data to server', error);
+        console.error('Login Failed', error);
     }
     
 
--- p-3/server/server.js
+++ p-3/server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const cors = require('cors');
 require('dotenv').config();
 const mongoDBURL = process.env.MONGO_URL;
 const app = express();
-
+app.use(cors());
 // for parsing JSON
 app.use(express.json());
 
@@ -40,4 +41,20 @@ app.get('/users', async (req, res) => {
     const users = await User.find();
     res.json(users);
 });
+
+// Login check
+app.post('/login', async (req, res) => {
+    const {username, email} = req.body;
+
+    try {
+        const user = await User.findOne({username, email});
+        if(!user){
+            return res.status(401).json({message : 'Invalid username or email'});
+        }
+        // if found return success
+        res.status(200).json({message : 'Login successful', user});
+    } catch (error) {
+        res.status(500).json({message : 'Server error', error : error.message});
+    }
+ });
 
