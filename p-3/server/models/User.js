61adf0bc60d507d92544682e6622e967decc7ac6
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-06-08 22:21:44 -0600

    server is conncted to mongoDB and i can creat and read my data of user

--- p-3/server/models/User.js
+++ p-3/server/models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const usertestSchema = new mongoose.Schema({
     username: String,
     email: String
 }, {timestamps: true});
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('Usertest', usertestSchema);
 
 module.exports = User;
--- p-3/server/server.js
+++ p-3/server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 require('dotenv').config();
-const mongoDBURL = process.env.mongoURL;
+const mongoDBURL = process.env.MONGO_URL;
 const app = express();
 
 // for parsing JSON
@@ -21,22 +22,22 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-let users = [];
 
 // Create POST http method
-app.post('/users', (req, res) => {
-    const user = req.body;
-    user.id = Date.now();
-    users.push(user);
-    res.status(201).json(user);
+app.post('/users', async (req, res) => {
+    try {
+        const user = await User(req.body);
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+    
 });
 
 // Read GET http method
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const users = await User.find();
     res.json(users);
 });
 
-// Update Put http method
-app.put('/users/:id', (req, res) => {
-
-});
